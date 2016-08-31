89ff3f748ca7f04c98891aeca868d99e74986b6f
Noam Tamim <noam.tamim@kaltura.com>
2016-08-31 21:48:18 +0300

    Share a file, not text.

--- app/src/main/java/com/kaltura/kalturadeviceinfo/MainActivity.java
+++ app/src/main/java/com/kaltura/kalturadeviceinfo/MainActivity.java
@@ -1,18 +1,26 @@
 package com.kaltura.kalturadeviceinfo;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -64,14 +72,53 @@ public class MainActivity extends AppCompatActivity {
             
             @Override
             public void onClick(View view) {
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, report);
-                sendIntent.putExtra(Intent.EXTRA_SUBJECT, "Kaltura Device Info - " + Build.BRAND + "/" + Build.MODEL + "/" + Build.VERSION.RELEASE + "/" + Build.VERSION.SDK_INT);
-                sendIntent.setType("application/json");
-                startActivity(Intent.createChooser(sendIntent, getResources().getText(R.string.send_to)));
+
+                String subject = "Kaltura Device Info - " + Build.BRAND + "/" + Build.MODEL + "/" + Build.VERSION.RELEASE + "/" + Build.VERSION.SDK_INT;
+                Intent shareIntent = intentWithAttachment(subject, report);
+                if (shareIntent == null) {
+                    Toast.makeText(MainActivity.this, "Can't share report as attachment", Toast.LENGTH_SHORT).show();
+                    shareIntent = intentWithText(subject, report);
+                }
+
+                startActivity(Intent.createChooser(shareIntent, getResources().getText(R.string.send_to)));
             }
         });
 
+    }
+
+    private Intent intentWithAttachment(String subject, String report) {
+        File reportsDir = new File(getFilesDir(), "reports");
+        reportsDir.mkdirs();
+        File reportFile = new File(reportsDir, "report.json");
+        try {
+            FileWriter writer = new FileWriter(reportFile);
+            writer.write(report);
+            writer.close();
+        } catch (IOException e) {
+            Log.e("ERROR", "Error creating report file", e);
+            return null;
+        }
+        Uri fileUri = FileProvider.getUriForFile(MainActivity.this, "com.kaltura.kalturadeviceinfo.fileprovider", reportFile);
+
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("application/json");
+        shareIntent.putExtra(Intent.EXTRA_STREAM, fileUri);
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+
+        PackageManager packageManager = getPackageManager();
+        if (shareIntent.resolveActivity(packageManager) == null) {
+            return null;
+        }
+        
+        return shareIntent;
+    }
+
+    private Intent intentWithText(String subject, String report) {
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, report);
+        sendIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        sendIntent.setType("text/plain");
+        return sendIntent;
     }
 }
